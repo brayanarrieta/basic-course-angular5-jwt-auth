5435915ec5c62714099fb5f9c3178d7ac1d1093e
Brayan Arrieta Alfaro <brayan.arrieta.alfaro.96@gmail.com>
2018-02-23 10:07:31 -0600

    Collect failed request for token expired

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import {User} from '../user';
 import {Router} from '@angular/router';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Authorization} from './authorization';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpRequest} from '@angular/common/http';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
 @Injectable()
 export class AuthService {
+  private cachedRequests: Array<HttpRequest<any>> = [];
   private authorization = new BehaviorSubject<Authorization>(null);
   constructor(private router: Router, private httpClient: HttpClient) { }
 
@@ -30,8 +31,24 @@ export class AuthService {
         (error ) => console.error(error)
       );
   }
+  refresh(): void {
+    this.httpClient.post<Authorization>('http://localhost:8000/api/auth/refresh', null, httpOptions )
+      .subscribe(
+        (data) => {
+          this.setAuthorization(data);
+        },
+        (error ) => console.error(error)
+      );
+  }
   logout() {
     this.authorization.next(null);
     this.router.navigate(['/login']);
+  }
+  public collectFailedRequest(request): void {
+    this.cachedRequests.push(request);
+  }
+  public retryFailedRequests(): void {
+    // retry the requests. this method can
+    // be called after the token is refreshed
   }
 }
